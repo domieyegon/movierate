095339c0b2fee92aa28cbaaba63e4b71302fab7b
Dominic Yegon <dyegon@maliora.tech>
2021-06-26 12:54:24 +0300

    Parent child communication

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,4 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { IMovie } from '../model/movie';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  movies?:IMovie[] = [];
+  selectedMovie = null;
+
+  constructor(
+    private apiService:ApiService
+  ) { }
 
   ngOnInit(): void {
+
+    this.apiService.getMovies().subscribe((res: HttpResponse<IMovie[]>) => (this.movies = res.body || []));
+    
   }
 
 }
--- frontend/src/app/main/movie-list/movie-list.component.ts
+++ frontend/src/app/main/movie-list/movie-list.component.ts
@@ -1,7 +1,5 @@
-import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api.service';
-import { IMovie, Movie } from 'src/app/model/movie';
+import { Component, Input, OnInit } from '@angular/core';
+import { IMovie } from 'src/app/model/movie';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,31 +8,10 @@ import { IMovie, Movie } from 'src/app/model/movie';
 })
 export class MovieListComponent implements OnInit {
 
-  movies?:IMovie[] = [];
+  @Input() movies?:IMovie[] = [];
 
+  constructor() { }
 
-  constructor(
-    private apiService:ApiService
-  ) { }
-
-
-
-
-  ngOnInit(): void {
-
-    this.apiService.getMovies().subscribe((res: HttpResponse<IMovie[]>) => (this.movies = res.body || []));
-    
-    // this.apiService.getMovies().subscribe(
-    //   data => {
-    //     console.log(data);
-        
-    //     this.movies = data;
-    //   },
-    //   error=> {
-    //   console.log(error);
-        
-    //   }
-    // );
-  }
+  ngOnInit(): void {}
 
 }
