4fd5bfd9421efab822cf8bae0c36ba09ce6fcc01
Dominic Yegon <dyegon@maliora.tech>
2021-07-02 22:08:04 +0300

    Added bootstrap 4

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { faFilm, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { MatDialog } from '@angular/material/dialog';
-import { MovieFormComponent } from './main/movie-form/movie-form.component';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +11,5 @@ export class AppComponent {
   film =faFilm;
 
   faLogout = faSignOutAlt
-
-  constructor(private dialog: MatDialog){}
-
-  openDialog() {
-    this.dialog.open(MovieFormComponent);
-  }
 
 }
